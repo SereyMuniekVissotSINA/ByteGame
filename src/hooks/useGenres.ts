077f3aa09c8b7dce7c9522d3719a6bb3bfecbb2b
SereyMuniekVissot SINA <vissot004@gmail.com>
2026-01-07 18:40:45 -0600

    Ship genres with Data

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,12 +1,12 @@
-import useData from "./useData";
+import genres from '../data/genres'
 
 export interface Genre {
   id: number;
   name: string;
-  image_background: string;
+  background_image: string;
 }
 
 
-const useGenre = () => useData<Genre>('/genres')
+const useGenre = () => ({data: genres, isLoading: false, error: null })
 
 export default useGenre;
