b69eac17c0d78f57fac64708fa206ef161802b6c
SereyMuniekVissot SINA <vissot004@gmail.com>
2026-01-16 11:42:21 -0600

    Fetching Genre using React Query

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -3,7 +3,7 @@ import apiClient from "../services/api-client";
 import { Axios, AxiosRequestConfig, CanceledError } from "axios";
 
 
-interface FetchResponse<T> {
+export interface FetchResponse<T> {
   count: number;
   results: T[];
 }
