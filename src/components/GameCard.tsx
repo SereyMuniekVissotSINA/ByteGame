3b5d9d035100e340700539a74226b9fc696f4e20
SereyMuniekVissot SINA <vissot004@gmail.com>
2026-01-27 14:12:57 -0600

    Refactoring Entities

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,11 +1,10 @@
-import { Card, CardBody, Heading, HStack, Image, Text } from "@chakra-ui/react";
-import { Game } from "../entities/Game";
+import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
+import Game  from "../entities/Game";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
 import Emoji from "./Emoji";
 import { Link } from "react-router-dom";
-import { transform } from "framer-motion";
 
 interface Props{
     game: Game
